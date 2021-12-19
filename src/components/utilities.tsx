da8d49da62a898379314b9af7657eb547f4158d4
shepard_liu <1057927664@qq.com>
2021-12-19 19:58:04 +0800

    fixed some bugs

--- src/components/utilities.tsx
+++ src/components/utilities.tsx
@@ -3,7 +3,7 @@ import './utilities.scss';
 import React from 'react';
 
 const RMBIcon: React.FunctionComponent<any> = (props) => {
-    return <img className={(props.className || '') + " rmg-sign "} src="../assets/rmb.svg" />;
+    return <svg className='rmb-sign' viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2216" width="200" height="200"><path d="M469.333333 682.666667v-85.333334H256v-85.333333h213.333333v-34.133333L264.533333 273.066667 328.533333 213.333333 512 396.8 695.466667 213.333333l59.733333 59.733334-200.533333 204.8v34.133333h213.333333v85.333333h-213.333333v85.333334h213.333333v85.333333h-213.333333v85.333333h-85.333334v-85.333333H256v-85.333333h213.333333z" fill="#969090" p-id="2217"></path></svg>
 };
 
 export { RMBIcon };
